b9f23a7ae4562b0b39081388c8a10ccc3952c080
Rungbam(main) <marungbam@naver.com>
2023-10-01 22:00:51 +0900

    mouse click and character move

--- homework.py
+++ homework.py
@@ -7,8 +7,9 @@ tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 hand_arrow = load_image('hand_arrow.png')
 
+click_positions = []
 character_x, character_y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-
+t = 0
 def handle_events():
     global running
 
@@ -22,6 +23,18 @@ def handle_events():
 
     pass
 
+def mouse_events():
+    global running
+    global click_positions
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                click_positions.append((event.x, TUK_HEIGHT - event.y))
+
 running = True
 frame = 0
 
@@ -31,7 +44,23 @@ while running:
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, character_x, character_y)
 
+    for pos in click_positions:
+        hand_arrow_x, hand_arrow_y = pos
+        hand_arrow.draw(hand_arrow_x, hand_arrow_y)
+
+    if len(click_positions) > 0:
+        hand_arrow_x, hand_arrow_y = click_positions[0]
+
+        if t < 1:
+            character_x = (1 - t) * character_x + t * hand_arrow_x
+            character_y = (1 - t) * character_y + t * hand_arrow_y
+            t += 0.02
+        else:
+            t = 0
+            click_positions.pop(0)
+
     update_canvas()
+    mouse_events()
     handle_events()
     frame = (frame + 1) % 8
     delay(0.05)
